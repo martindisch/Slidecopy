9fd8c5aebdc78be8646ff6851a3b96dd6f776518
Martin Disch <martindisch@gmail.com>
2016-11-01 10:34:51 +0100

    Basic settings form

---
+++ windows/Slidecopy/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Slidecopy
+{
+    partial class FormSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelCode = new System.Windows.Forms.Label();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.contextMenuStripIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStripIcon;
+            this.notifyIcon.Text = "Slidecopy";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStripIcon
+            // 
+            this.contextMenuStripIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemSettings,
+            this.toolStripMenuItemExit});
+            this.contextMenuStripIcon.Name = "contextMenuStripIcon";
+            this.contextMenuStripIcon.Size = new System.Drawing.Size(117, 48);
+            // 
+            // toolStripMenuItemSettings
+            // 
+            this.toolStripMenuItemSettings.Name = "toolStripMenuItemSettings";
+            this.toolStripMenuItemSettings.Size = new System.Drawing.Size(116, 22);
+            this.toolStripMenuItemSettings.Text = "Settings";
+            this.toolStripMenuItemSettings.Click += new System.EventHandler(this.toolStripMenuItemSettings_Click);
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(116, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // labelCode
+            // 
+            this.labelCode.AutoSize = true;
+            this.labelCode.Location = new System.Drawing.Point(13, 13);
+            this.labelCode.Name = "labelCode";
+            this.labelCode.Size = new System.Drawing.Size(113, 13);
+            this.labelCode.TabIndex = 0;
+            this.labelCode.Text = "Unique code (Android)";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(16, 29);
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(110, 20);
+            this.textBoxCode.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(132, 27);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Location = new System.Drawing.Point(16, 56);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(51, 13);
+            this.labelIP.TabIndex = 4;
+            this.labelIP.Text = "Server IP";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(16, 73);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(110, 20);
+            this.textBoxIP.TabIndex = 5;
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(133, 56);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(59, 13);
+            this.labelPort.TabIndex = 6;
+            this.labelPort.Text = "Server port";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(136, 73);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(71, 20);
+            this.textBoxPort.TabIndex = 7;
+            // 
+            // FormSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 109);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.labelIP);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.labelCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slidecopy";
+            this.contextMenuStripIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label labelCode;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripIcon;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSettings;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.TextBox textBoxPort;
+    }
+}
+
---
+++ windows/Slidecopy/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Slidecopy
+{
+    public partial class FormSettings : Form
+    {
+        public FormSettings()
+        {
+            InitializeComponent();
+            notifyIcon.Icon = SystemIcons.Application;
+            notifyIcon.DoubleClick += new EventHandler(notifyIcon_DoubleClick);
+            this.Resize += new EventHandler(FormSettings_Resize);
+        }
+
+        void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            // TODO: Download & clipboard
+        }
+
+        void FormSettings_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized) Hide();
+        }
+
+        private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void toolStripMenuItemSettings_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.BringToFront();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
