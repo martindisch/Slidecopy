b33eb97d4402f12b466aa17010a49ad84ac8d119
Martin Disch <martindisch@gmail.com>
2016-11-01 15:27:20 +0100

    Download image and copy to clipboard

--- windows/Slidecopy/Form1.cs
+++ windows/Slidecopy/Form1.cs
@@ -3,12 +3,14 @@ using System.Drawing;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using System.Text.RegularExpressions;
+using System.Net;
 
 namespace Slidecopy
 {
     public partial class FormSettings : Form
     {
         private bool gotData = false;
+        private string code, ip, port;
 
         public FormSettings()
         {
@@ -21,9 +23,9 @@ namespace Slidecopy
             RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Software\Slidecopy");
             if (regKey != null)
             {
-                string code = (string) regKey.GetValue("code");
-                string ip = (string) regKey.GetValue("ip");
-                string port = (string) regKey.GetValue("port");
+                code = (string) regKey.GetValue("code");
+                ip = (string) regKey.GetValue("ip");
+                port = (string) regKey.GetValue("port");
                 textBoxCode.Text = code;
                 textBoxIP.Text = ip;
                 textBoxPort.Text = port;
@@ -39,8 +41,32 @@ namespace Slidecopy
         {
             if (gotData)
             {
-                // download image
-
+                // build request
+                string url = @"http://" + ip + @":" + port + @"/photo/" + code + @".jpg";
+                WebRequest request = WebRequest.Create(url);
+                // show downloading notification
+                notifyIcon.ShowBalloonTip(0, null, "Downloading image...", ToolTipIcon.Info);
+                try
+                { 
+                    WebResponse response = request.GetResponse();
+                    System.IO.Stream responseStream = response.GetResponseStream();
+                    // build bitmap from downloaded data
+                    Bitmap image = new Bitmap(responseStream);
+                    // put image in clipboard
+                    Clipboard.SetImage(image);
+                    // release bitmap
+                    image.Dispose();
+                    // ugly hack to clear old balloonTip
+                    notifyIcon.Visible = false;
+                    notifyIcon.Visible = true;
+                    notifyIcon.ShowBalloonTip(0, null, "Download complete", ToolTipIcon.Info);
+                } catch (WebException)
+                {
+                    // ugly hack to clear old balloonTip
+                    notifyIcon.Visible = false;
+                    notifyIcon.Visible = true;
+                    notifyIcon.ShowBalloonTip(0, null, "Download failed", ToolTipIcon.Error);
+                }
             } else
             {
                 // if no server settings available, show input form to user
@@ -60,10 +86,11 @@ namespace Slidecopy
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
+            gotData = false;
             // check input for errors
-            string code = textBoxCode.Text;
-            string ip = textBoxIP.Text;
-            string port = textBoxPort.Text;
+            code = textBoxCode.Text;
+            ip = textBoxIP.Text;
+            port = textBoxPort.Text;
             Regex rgxPositiveInt = new Regex(@"^[1-9]\d*$");
             Regex rgxIP = new Regex(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
             if (!rgxPositiveInt.IsMatch(code))
