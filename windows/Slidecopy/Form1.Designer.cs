7f85397bf501a559171be82cc39fa44ddf070bcd
Martin Disch <martindisch@gmail.com>
2016-11-05 15:11:54 +0100

    New icon on Windows

--- windows/Slidecopy/Form1.Designer.cs
+++ windows/Slidecopy/Form1.Designer.cs
@@ -50,47 +50,51 @@
             // 
             // contextMenuStripIcon
             // 
+            this.contextMenuStripIcon.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStripIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItemSettings,
             this.toolStripMenuItemExit});
             this.contextMenuStripIcon.Name = "contextMenuStripIcon";
-            this.contextMenuStripIcon.Size = new System.Drawing.Size(117, 48);
+            this.contextMenuStripIcon.Size = new System.Drawing.Size(138, 56);
             // 
             // toolStripMenuItemSettings
             // 
             this.toolStripMenuItemSettings.Name = "toolStripMenuItemSettings";
-            this.toolStripMenuItemSettings.Size = new System.Drawing.Size(116, 22);
+            this.toolStripMenuItemSettings.Size = new System.Drawing.Size(137, 26);
             this.toolStripMenuItemSettings.Text = "Settings";
             this.toolStripMenuItemSettings.Click += new System.EventHandler(this.toolStripMenuItemSettings_Click);
             // 
             // toolStripMenuItemExit
             // 
             this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
-            this.toolStripMenuItemExit.Size = new System.Drawing.Size(116, 22);
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(137, 26);
             this.toolStripMenuItemExit.Text = "Exit";
             this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
             // 
             // labelCode
             // 
             this.labelCode.AutoSize = true;
-            this.labelCode.Location = new System.Drawing.Point(13, 13);
+            this.labelCode.Location = new System.Drawing.Point(17, 16);
+            this.labelCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCode.Name = "labelCode";
-            this.labelCode.Size = new System.Drawing.Size(166, 13);
+            this.labelCode.Size = new System.Drawing.Size(223, 17);
             this.labelCode.TabIndex = 0;
             this.labelCode.Text = "Unique code (same as in Android)";
             // 
             // textBoxCode
             // 
-            this.textBoxCode.Location = new System.Drawing.Point(16, 29);
+            this.textBoxCode.Location = new System.Drawing.Point(21, 36);
+            this.textBoxCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxCode.Name = "textBoxCode";
-            this.textBoxCode.Size = new System.Drawing.Size(110, 20);
+            this.textBoxCode.Size = new System.Drawing.Size(145, 22);
             this.textBoxCode.TabIndex = 0;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(132, 27);
+            this.buttonSave.Location = new System.Drawing.Point(176, 33);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(100, 28);
             this.buttonSave.TabIndex = 2;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -98,24 +102,26 @@
             // 
             // labelDoubleClick
             // 
-            this.labelDoubleClick.Location = new System.Drawing.Point(12, 64);
+            this.labelDoubleClick.Location = new System.Drawing.Point(16, 79);
+            this.labelDoubleClick.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelDoubleClick.Name = "labelDoubleClick";
-            this.labelDoubleClick.Size = new System.Drawing.Size(194, 47);
+            this.labelDoubleClick.Size = new System.Drawing.Size(259, 58);
             this.labelDoubleClick.TabIndex = 7;
             this.labelDoubleClick.Text = "Double click the icon in the notification area to copy your current photo to clip" +
     "board";
             // 
             // FormSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(221, 119);
+            this.ClientSize = new System.Drawing.Size(295, 146);
             this.Controls.Add(this.labelDoubleClick);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.textBoxCode);
             this.Controls.Add(this.labelCode);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormSettings";
